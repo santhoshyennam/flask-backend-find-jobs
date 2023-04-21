8ffa0054c600b6307b4c5aa36ed02c1ed6822431
Alapati_Lokesh <l.alapati@eagles.oc.edu>
2023-04-20 23:00:35 -0400

    changes in signup flow

--- app/main/authentication.py
+++ app/main/authentication.py
@@ -25,7 +25,7 @@ def login():
                     'is_manager': True,
                     'exp': datetime.datetime.utcnow().timestamp() + 3600
                 }, SECRET_KEY)
-                return Response(json.dumps({'access_token': token, 'manager': manager.to_dict()})), 200
+                return Response(json.dumps({'access_token': token, 'manager': manager.to_dict(), "is_manager":True})), 200
             else:  # user is trying to login
                 user = User.query.filter_by(email=request.json["email"]).first()
                 if not user or not check_password_hash(user.password, request.json["password"]):
@@ -35,22 +35,32 @@ def login():
                     'is_manager': False,
                     'exp': datetime.datetime.utcnow().timestamp() + 3600
                 }, SECRET_KEY)
-                return Response(json.dumps({'access_token': token, 'user': user.to_dict()})), 200
+                return Response(json.dumps({'access_token': token, 'user': user.to_dict(), "is_manager": False})), 200
     else:
         return generate_error_response("some fields are missing in the request")
 
 # signup for user
 @main.route("/signup", methods=["POST"])
 def signup():
-    if "name" in request.json and "mobile" in request.json and "age" in request.json and "email" in request.json and "address" in request.json and "date_of_birth" in request.json and "password" in request.json:
+    if "name" in request.json and "mobile" in request.json and "age" in request.json and "email" in request.json and "address" in request.json and "date_of_birth" in request.json and "password" in request.json and "is_manager" in request.json:
         data = request.get_json()
-        user = User.query.filter_by(email=request.json["email"]).first()
-        if user:
-            return generate_error_response('Email is already taken'), 400
-        new_user = User(name=data['name'], age=data['age'], mobile=data["mobile"], email=data["email"], address=data["address"], date_of_birth=data["date_of_birth"],password=generate_password_hash(data["password"]))
-        db.session.add(new_user)
-        db.session.commit()
-        return Response(json.dumps(new_user.to_dict()), mimetype='application/json'),201
+        if data["is_manager"] == True:
+            manager = Manager.query.filter_by(email=request.json["email"]).first()
+            if manager:
+                return generate_error_response('Email is already taken'), 400
+            new_manager = Manager(name=data['name'], age=data['age'], mobile=data["mobile"], email=data["email"], address=data["address"], date_of_birth=data["date_of_birth"],password=generate_password_hash(data["password"]))
+            db.session.add(new_manager)
+            db.session.commit()
+            return Response(json.dumps(new_manager.to_dict()), mimetype='application/json'),201
+        else:
+            user = User.query.filter_by(email=request.json["email"]).first()
+            if user:
+                return generate_error_response('Email is already taken'), 400
+            new_user = User(name=data['name'], age=data['age'], mobile=data["mobile"], email=data["email"], address=data["address"], date_of_birth=data["date_of_birth"],password=generate_password_hash(data["password"]))
+            db.session.add(new_user)
+            db.session.commit()
+            return Response(json.dumps(new_user.to_dict()), mimetype='application/json'),201
+
     else:
         return generate_error_response("some fields are missing in the request")
 
