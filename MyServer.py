4819696db151536f5f3df170dc66c64cbf090cd5
santhoshyennam <sandysanthosh045@gmail.com>
2023-05-04 20:47:15 -0400

    add docker file

--- MyServer.py
+++ MyServer.py
@@ -4,3 +4,6 @@ from flask_migrate import Migrate
 
 app = create_app(os.getenv("FLASK_CONFIG") or 'default')
 migrate = Migrate(app,db)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001,debug=True)
